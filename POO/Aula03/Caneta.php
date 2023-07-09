46a4e7aebb811b444de020a1deb5a8b9e3d8d744
arthur201306 <arthurpaivamuniz@gmail.com>
2023-07-09 19:04:24 -0300

    adicionado aula03 do curso de poo

---
+++ POO/Aula03/Caneta.php
@@ -0,0 +1,25 @@
+<?php 
+  class Caneta {
+    public $modelo;
+    public $cor;
+    private $ponta;
+    protected $carga;
+    protected $tampada;
+
+    public function rabiscar() {
+      if ($this->$tampada) {
+        echo "<p>[ERRO] Não posso rabiscar</p>";
+      } else {
+        echo "<p>Estou rabiscando !</p>"
+      }
+    }
+
+    private function tampar() {
+      $this->$tampada = true;
+    }
+
+    private function destampar() {
+      $this->$tampada = false;
+    }
+  }
+?>
