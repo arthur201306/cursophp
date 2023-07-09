8e7d51b7106cae5a89c77172d433162ab1dece55
arthur201306 <arthurpaivamuniz@gmail.com>
2023-07-09 16:06:13 -0300

    adicionado poo

---
+++ POO/Aula02/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Testando classes</title>
+</head>
+<body>
+  <h1>Teste de classes</h1>
+  <pre>
+    <?php 
+      require_once 'Caneta.php';
+
+      $c1 = new Caneta;
+      $c1->cor = "Azul";
+      $c1->tampada = false;
+      $c1->destampar();
+      $c1->rabiscar();
+    ?>
+  </pre>
+</body>
+</html>
