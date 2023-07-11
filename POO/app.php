6e2d201235ba96cf0dc23207d6cb03466651985c
arthur201306 <arthurpaivamuniz@gmail.com>
2023-07-10 21:27:48 -0300

    Atualização POO PHP

---
+++ POO/app.php
@@ -0,0 +1,10 @@
+<?php 
+
+  require_once __DIR__ . "./src/PessoaFisica.php";
+
+  use Poo\Arthur\PessoaFisica;
+
+  $pessoaFisica = new PessoaFisica();
+
+  dump($pessoaFisica);
+?>
---
+++ POO/src/PessoaFisica.php
@@ -0,0 +1,11 @@
+<?php
+  namespace Poo\Arthur;
+
+  class PessoaFisica {
+    public $nome;
+  }
+
+  $pessoaFisica = new PessoaFisica();
+
+  var_dump($pessoaFisica);
+?>
